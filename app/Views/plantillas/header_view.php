a6e94e33b5922292d4ad8d117c5a9c07a3044ef2
EnzoBarrios <EnzoBarrios2018@gmail.com>
2025-04-09 15:13:15 -0300

    Estructura del proyecto

---
+++ app/Views/plantillas/header_view.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initialscale=1,shrink-to-fit=no">
+    <title>Mate-Store</title>
+    <link rel="stylesheet" href="<?php echo base_url('/assets/css/bootstrap.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('/assets/css/style.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('/assets/js/bootstrap.min.js'); ?>">
+</head>
+
+<body>
---
+++ app/Views/plantillas/nav_view.php
@@ -0,0 +1,22 @@
+<h1>Hello, world!</h1>
+
+<!-- Contenedor principal -->
+<section class="container-fluid">
+
+    <!-- Barra de navegación -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Mi Sitio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="#">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Servicios</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Contacto</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</section>
