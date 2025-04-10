054e998e5b26da3c731958b289b16fbc61e12430
EnzoBarrios <EnzoBarrios2018@gmail.com>
2025-04-10 10:50:41 -0300

    Diseño del pie de página con estilo responsivo y enlaces

--- app/Views/contenido/comercializacion.php
+++ app/Views/contenido/comercializacion.php
@@ -1,1 +1,2 @@
 <h1>Comercialización</h1>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
--- app/Views/plantillas/footer_view.php
+++ app/Views/plantillas/footer_view.php
@@ -1,7 +1,47 @@
 <!-- 🔹 Pie de página -->
-<footer class="bg-dark text-white text-center py-3 mt-5">
-    <p class="mb-0">© 2025 Mi Sitio Web. Todos los derechos reservados.</p>
-</footer>
-</body>
+<footer class="bg-dark text-white pt-5 pb-3 mt-5">
+    <div class="container">
+        <div class="row text-center text-md-start">
+            <!-- Logo o nombre -->
+            <div class="col-md-3 mb-4">
+                <h5 class="text-uppercase">Mate Store</h5>
+                <p>La tradición hecha diseño. Productos únicos para compartir buenos momentos.</p>
+            </div>
 
-</html>
+            <!-- Navegación -->
+            <div class="col-md-3 mb-4">
+                <h6 class="text-uppercase">Navegación</h6>
+                <ul class="list-unstyled">
+                    <li><a href="<?= base_url('inicio'); ?>" class="text-white text-decoration-none">Inicio</a></li>
+                    <li><a href="<?= base_url('comercializacion'); ?>" class="text-white text-decoration-none">Comercialización</a></li>
+                    <li><a href="<?= base_url('quienes-somos'); ?>" class="text-white text-decoration-none">¿Quiénes Somos?</a></li>
+                    <li><a href="<?= base_url('terminos-y-condiciones'); ?>" class="text-white text-decoration-none">Términos y Condiciones</a></li>
+                    <li><a href="<?= base_url('contacto'); ?>" class="text-white text-decoration-none">Contacto</a></li>
+                </ul>
+            </div>
+
+            <!-- Contacto -->
+            <div class="col-md-3 mb-4">
+                <h6 class="text-uppercase">Contacto</h6>
+                <p>Email: contacto@matestore.com</p>
+                <p>Teléfono: +54 9 1234 567890</p>
+                <p>Corrientes, Argentina</p>
+            </div>
+
+            <!-- Redes sociales -->
+            <div class="col-md-3 mb-4">
+                <h6 class="text-uppercase">Seguinos</h6>
+                <div class="d-flex flex-column">
+                    <a href="#" class="text-white text-decoration-none mb-2"><i class="fab fa-facebook-f me-2"></i> Mate.store</a>
+                    <a href="#" class="text-white text-decoration-none mb-2"><i class="fab fa-instagram me-2"></i> Mate.store</a>
+                    <a href="#" class="text-white text-decoration-none"><i class="fab fa-twitter me-2"></i> Mate.store</a>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="text-center mt-3 border-top pt-3">
+            <p class="mb-0">© 2025 Mate Store. Todos los derechos reservados.</p>
+        </div>
+    </div>
+</footer>
