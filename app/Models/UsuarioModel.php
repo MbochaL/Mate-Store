e5a140a451f6caa230df861197a3649e32a267d5
MbochaL <martinbosch1996@gmail.com>
2025-05-15 11:33:03 -0300

    add: sql models

---
+++ app/Models/UsuarioModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model{
+    protected $table      = 'usuario';
+    protected $primaryKey = 'id_usuario';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array'; //object
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = ['id_usuario','apellido_usuario', 'nombre_usuario', 'telefono_usuario', 'mail_usuario', 'dni_usuario', 'direccion_usuario', 'password_usuario', 'estado_usuario', 'id_rol'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+}
